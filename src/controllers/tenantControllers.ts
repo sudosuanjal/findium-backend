081e2b465fa5ce2c53e052cfcf591a4d3cfc3e0f
sudosuanjal <anjalehackchey101@gmail.com>
2025-09-13 15:51:30 +0530

    feat:createTenant  route

--- src/controllers/tenantControllers.ts
+++ src/controllers/tenantControllers.ts
@@ -19,10 +19,31 @@ export const getTenant = async (req: Request, res: Response): Promise<void> => {
       res.status(404).json({ message: "tenant not found" });
     }
   } catch (error: any) {
+    res.status(500).json({
+      message: `server error while retreving tenant: &{error.message}`,
+    });
+  }
+};
+
+export const createTenant = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const { cognitoId, name, email, phoneNumber } = req.body;
+
+    const tenant = await prisma.tenant.create({
+      data: cognitoId,
+      name,
+      email,
+      phoneNumber,
+    });
+
+    res.status(201).json(tenant);
+  } catch (error: any) {
+    console.error(`error while createing tenant: ${error.message}`);
     res
       .status(500)
-      .json({
-        message: `server error while retreving tenant: &{error.message}`,
-      });
+      .json({ message: `error while createing tenant: ${error.message}` });
   }
 };
