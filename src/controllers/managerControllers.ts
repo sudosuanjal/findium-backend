7a3c7cfb406ad7897d378c974229cc221e365a35
sudosuanjal <anjalehackchey101@gmail.com>
2025-09-15 23:22:14 +0530

    feat:manager controller  route

---
+++ src/controllers/managerControllers.ts
@@ -0,0 +1,50 @@
+import { PrismaClient } from "@prisma/client/extension";
+import { Request, Response } from "express";
+
+const prisma = new PrismaClient();
+
+export const getManager = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const { cognitoId } = req.params;
+    const manager = await prisma.manager.findUnique({
+      where: { cognitoId },
+    });
+
+    if (manager) {
+      res.json(manager);
+    } else {
+      res.status(404).json({ message: "manager does not exist" });
+    }
+  } catch (error: any) {
+    res
+      .status(500)
+      .json({ message: `error retriving managers: ${error.message}` });
+  }
+};
+
+export const createManager = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const { cognitoId, name, email, phoneNumber } = req.body;
+
+    const manager = await prisma.manager.create({
+      data: {
+        cognitoId,
+        name,
+        email,
+        phoneNumber,
+      },
+    });
+
+    res.status(201).json(manager);
+  } catch (error: any) {
+    res
+      .status(500)
+      .json({ message: `error creating the manager: ${error.message}` });
+  }
+};
